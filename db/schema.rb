6b39481d16229a684a7d11d02781c5ee2df89629
Vivien-Ploix <vivien.ploix@gmail.com>
2020-09-07 23:15:20 +0200

    migration items

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_07_204140) do
+ActiveRecord::Schema.define(version: 2020_09_07_211107) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,6 +35,18 @@ ActiveRecord::Schema.define(version: 2020_09_07_204140) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_consumers_on_email", unique: true
     t.index ["reset_password_token"], name: "index_consumers_on_reset_password_token", unique: true
+  end
+
+  create_table "items", force: :cascade do |t|
+    t.string "title"
+    t.decimal "price"
+    t.boolean "available"
+    t.text "description"
+    t.string "category"
+    t.bigint "business_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["business_id"], name: "index_items_on_business_id"
   end
 
   create_table "owners", force: :cascade do |t|
