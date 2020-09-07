4b6b144d07fd802933df6f6bd7488ae71deeda7b
Vivien-Ploix <vivien.ploix@gmail.com>
2020-09-07 22:35:53 +0200

    add model Business

--- app/models/owner.rb
+++ app/models/owner.rb
@@ -1,6 +1,12 @@
 class Owner < ApplicationRecord
+
+  has_many :businesses
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_05_093605) do
+ActiveRecord::Schema.define(version: 2020_09_07_202645) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "businesses", force: :cascade do |t|
+    t.string "name"
+    t.string "address"
+    t.string "siret"
+    t.bigint "owner_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["owner_id"], name: "index_businesses_on_owner_id"
+  end
 
   create_table "owners", force: :cascade do |t|
     t.string "email", default: "", null: false
