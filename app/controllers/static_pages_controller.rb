9430d5b7b998d2f5105887eb66b9737436e8b0b5
Vivien-Ploix <vivien.ploix@gmail.com>
2020-09-08 23:01:09 +0200

    fix issue template, template added, creation of new layout for crystal and static pages crystal_template and kit_ui

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,12 @@
 class StaticPagesController < ApplicationController
+  layout "crystal", only: [:crystal_template, :kit_ui]
+
   def home
   end
+
+  def crystal_template
+  end 
+
+  def kit_ui
+  end 
 end
