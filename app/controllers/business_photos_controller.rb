b99beeaa881071441fdb49e254c42a559de30358
StanislasBASQUIN <stanislas.basquin@gmail.com>
2020-09-11 11:41:01 +0200

    edition on business#index Front

--- app/controllers/business_photos_controller.rb
+++ app/controllers/business_photos_controller.rb
@@ -4,7 +4,11 @@ class BusinessPhotosController < ApplicationController
 
   def create
     @business = Business.find(params[:business_id])
-    @business.business_photo.attach(params[:business_photo])
-    redirect_to(business_path(@business))
-  end
+
+    if @business.business_photo.attach(params[:business_photo])
+      redirect_to(business_path(@business))
+    else 
+      flash[:danger] = "Vous ne pouvez pas effectuer cette action"
+      redirect_to(business_path(@business)) 
+    end
 end
