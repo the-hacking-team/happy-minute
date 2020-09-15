d5e3f22cba75a4b394b3d22081bdc1b1df8729fc
Vivien-Ploix <vivien.ploix@gmail.com>
2020-09-15 22:21:35 +0200

    modifications front : reorganizing page show: buttons on the right, phone number clickable. Button with the number of happy prices on business cards that leads directly to business_items

--- app/models/business.rb
+++ app/models/business.rb
@@ -8,4 +8,12 @@ class Business < ApplicationRecord
 
   validates :name, presence: true
   validates :address, presence: true
+
+  def number_happy_prices
+    i = 0
+    items.each do |item|
+      i += item.happy_prices.size
+    end
+    i
+  end
 end
