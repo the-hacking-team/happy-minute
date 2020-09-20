f563d2daf0ba36a175adb9e4496b3c7e6845805c
Mathieu Voland <mathieu.voland@gmail.com>
2020-09-20 09:25:02 +0200

    Test Facebook OmniAuth - devise tutorial

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_14_150705) do
+ActiveRecord::Schema.define(version: 2020_09_20_071044) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -85,6 +85,8 @@ ActiveRecord::Schema.define(version: 2020_09_14_150705) do
     t.datetime "updated_at", null: false
     t.string "first_name"
     t.string "last_name"
+    t.string "provider"
+    t.string "uid"
     t.index ["email"], name: "index_customers_on_email", unique: true
     t.index ["reset_password_token"], name: "index_customers_on_reset_password_token", unique: true
   end
