9c3266bedd4351b6e0ab293c5edc0ce50cab4286
Mathieu Voland <mathieu.voland@gmail.com>
2020-09-20 19:40:17 +0200

    Redirect to previous page after sign_in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   layout :layout_by_resource
 
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :store_location!, if: :storable_location?
 
   protected
 
@@ -18,5 +19,19 @@ class ApplicationController < ActionController::Base
     else
       'layouts/application'
     end
+  end
+
+  def storable_location?
+    request.get? && is_navigational_format? && !devise_controller? && !request.xhr?
+  end
+
+  def store_location!
+    # :user is the scope we are authenticating
+    store_location_for(:customer, request.fullpath)
+    store_location_for(:owner, request.fullpath)
+  end
+
+  def after_sign_in_path_for(resource_or_scope)
+    stored_location_for(resource_or_scope) || super
   end
 end
